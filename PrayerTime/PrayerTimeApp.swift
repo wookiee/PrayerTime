5cb561f4db2401f32fa639731f5af5b6e33cfce7
Mikey Ward <mw@mikey.io>
2024-01-31 05:59:08 -0500

    Initial Commit

---
+++ PrayerTime/PrayerTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimeApp.swift
+//  PrayerTime
+//
+//  Created by Mikey Ward on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
